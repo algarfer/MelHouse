9d4885f8483261518d543019b07218a505781892
algarfer <algarferx@proton.me>
2024-10-06 16:26:14 +0200

    Set up package structure

--- apk/app/src/main/java/com/uniovi/melhouse/presentation/view/MainActivity.kt
+++ apk/app/src/main/java/com/uniovi/melhouse/presentation/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.uniovi.melhouse
+package com.uniovi.melhouse.presentation.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uniovi.melhouse.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
