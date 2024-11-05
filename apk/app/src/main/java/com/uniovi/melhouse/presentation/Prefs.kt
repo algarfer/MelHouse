29e297533ef349e38508cbb828db5ae01bbb4b29
algarfer <algarferx@proton.me>
2024-11-05 15:32:11 +0100

    Added more data to shared prefs

--- apk/app/src/main/java/com/uniovi/melhouse/presentation/Prefs.kt
+++ apk/app/src/main/java/com/uniovi/melhouse/presentation/Prefs.kt
@@ -2,11 +2,14 @@ package com.uniovi.melhouse.presentation
 
 import android.content.Context
 import android.content.SharedPreferences
+import java.util.UUID
 
 object Prefs {
 
     private const val SHARED_NAME = "MelhousePrefs"
     private const val SHARED_USER_NAME = "username"
+    private const val SHARED_USER_ID = "user_id"
+    private const val SHARED_FLAT_ID = "flat_id"
 
     @Volatile
     private var storage: SharedPreferences? = null
@@ -22,5 +25,21 @@ object Prefs {
 
     fun getUserName(): String {
         return storage!!.getString(SHARED_USER_NAME, "")!!
+    }
+
+    fun setFlatId(newId: UUID) {
+        storage!!.edit().putString(SHARED_FLAT_ID, newId.toString()).apply()
+    }
+
+    fun getFlatId(): UUID {
+        return UUID.fromString(storage!!.getString(SHARED_FLAT_ID, "")!!)
+    }
+
+    fun setUserId(newId: UUID) {
+        storage!!.edit().putString(SHARED_USER_ID, newId.toString()).apply()
+    }
+
+    fun getUserId(): UUID {
+        return UUID.fromString(storage!!.getString(SHARED_USER_ID, "")!!)
     }
 }
