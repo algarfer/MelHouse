5738524fbbd2cd3e45c21603ad9a3ba117f25274
algarfer <algarferx@proton.me>
2024-11-05 15:10:46 +0100

    Fixed system bar color on menu activity

--- apk/app/src/main/java/com/uniovi/melhouse/presentation/view/MenuActivity.kt
+++ apk/app/src/main/java/com/uniovi/melhouse/presentation/view/MenuActivity.kt
@@ -1,21 +1,39 @@
 package com.uniovi.melhouse.presentation.view
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.uniovi.melhouse.R
+import com.uniovi.melhouse.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+    private var androidOsBarColor: Int? = null
+
+    @SuppressLint("PrivateResource")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        androidOsBarColor = window.navigationBarColor
+        // Review - This may not work with dark mode
+        window.navigationBarColor = getColor(com.google.android.material.R.color.m3_ref_palette_neutral94)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        window.navigationBarColor = androidOsBarColor!!
     }
 }
