60ae404bd4edac8eb87ac8459f55e41bfdbcd1ff
algarfer <algarferx@proton.me>
2024-11-06 13:56:06 +0100

    Added code for logout

--- apk/app/src/main/java/com/uniovi/melhouse/presentation/fragments/SettingsFragment.kt
+++ apk/app/src/main/java/com/uniovi/melhouse/presentation/fragments/SettingsFragment.kt
@@ -1,32 +1,48 @@
 package com.uniovi.melhouse.presentation.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.uniovi.melhouse.R
 import com.uniovi.melhouse.databinding.FragmentSettingsBinding
+import com.uniovi.melhouse.presentation.activities.NotRegisteredActivity
+import com.uniovi.melhouse.viewmodel.SettingViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
+    private val viewModel: SettingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel.isLogged.observe(this) {
+            if(it) return@observe
+
+            val activity = activity!!
+            activity.startActivity(Intent(activity, NotRegisteredActivity::class.java))
+            activity.finish()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSettingsBinding.bind(view)
+
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSettingsBinding.inflate(layoutInflater)
-        return binding.root
+        return layoutInflater.inflate(R.layout.fragment_settings, container, false)
     }
 
 }
---
+++ apk/app/src/main/java/com/uniovi/melhouse/viewmodel/SettingViewModel.kt
@@ -0,0 +1,19 @@
+package com.uniovi.melhouse.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.uniovi.melhouse.preference.Prefs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor() : ViewModel() {
+
+    val isLogged: MutableLiveData<Boolean> = MutableLiveData(true)
+
+    fun logout() {
+        Prefs.clearAll()
+
+        isLogged.postValue(false)
+    }
+}
