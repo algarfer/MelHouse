1e3b180a9fab9411c1061b96b28e827fdd1cb5bf
algarfer <algarferx@proton.me>
2024-11-16 14:52:16 +0100

    Added RLS for tasks and asignees

--- backend/supabase/db/init/scripts/50-RLS-task.sql
+++ backend/supabase/db/init/scripts/50-RLS-task.sql
@@ -1,1 +1,16 @@
 alter table public.tasks enable row level security;
+
+create policy "Allow user to manage tasks of his flat"
+on public.tasks
+as PERMISSIVE
+for ALL
+to authenticated
+using (
+    flat_id = (select flat_id
+    from public.users
+    where id = (select auth.uid())))
+with check (
+    flat_id = (select flat_id
+    from public.users
+    where id = (select auth.uid()))
+);
--- backend/supabase/db/init/scripts/50-RLS-tasks_users.sql
+++ backend/supabase/db/init/scripts/50-RLS-tasks_users.sql
@@ -1,1 +1,23 @@
 alter table public.tasks_users enable row level security;
+
+create policy "Allow user to manage asignees of his flat tasks"
+on public.tasks_users
+as PERMISSIVE
+for ALL
+to authenticated
+using (
+    (select flat_id
+    from public.tasks
+    where id = task_id) =
+    (select flat_id
+    from public.users
+    where id = (select auth.uid()))
+)
+with check (
+    (select flat_id
+    from public.tasks
+    where id = task_id) =
+    (select flat_id
+    from public.users
+    where id = (select auth.uid()))
+);
