b1201034c4d4d9b07e6c5cbde63c704ae503ff74
Alberto Lago Conde <uo288245@uniovi.es>
2024-11-16 14:22:52 +0100

    Añadido el fragmento para creación de pisos.

---
+++ apk/app/src/main/java/com/uniovi/melhouse/presentation/fragments/CreateFlatFragment.kt
@@ -0,0 +1,24 @@
+package com.uniovi.melhouse.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uniovi.melhouse.R
+
+class CreateFlatFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_flat, container, false)
+    }
+
+}
