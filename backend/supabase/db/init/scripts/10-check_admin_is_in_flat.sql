05d2f4e704de1d87e54c95423a7482dbd28a510a
algarfer <algarferx@proton.me>
2025-01-04 03:52:32 +0100

    Added a trigger to check that the new admin of a flat is really of that flat

---
+++ backend/supabase/db/init/scripts/10-check_admin_is_in_flat.sql
@@ -0,0 +1,21 @@
+create or replace function private.check_admin_in_flat()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+declare
+    f_id uuid;
+begin
+    select flat_id into f_id
+    from public.users
+    where id = new.admin_id;
+
+    assert f_id = new.id, 'admin_not_in_flat';
+
+    return new;
+end;
+$$;
+
+create or replace trigger trg_check_admin_in_flat
+before insert or update on public.flats
+for each row execute function private.check_admin_in_flat();
