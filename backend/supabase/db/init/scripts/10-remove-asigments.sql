5bc4a366cbd2d25eb1754b66fc29327816cd2282
algarfer <algarferx@proton.me>
2025-01-04 04:54:25 +0100

    Fixed remove assignments trigger again

--- backend/supabase/db/init/scripts/10-remove-asigments.sql
+++ backend/supabase/db/init/scripts/10-remove-asigments.sql
@@ -4,7 +4,7 @@ language plpgsql
 security definer set search_path = ''
 as $$
 begin
-    if old.flat_id <> new.flat_id then
+    if old.flat_id <> new.flat_id or new.flat_id is null then
         delete from public.tasks_users where user_id = old.id;
     end if;
     return new;
