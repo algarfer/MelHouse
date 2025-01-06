c9ba66125d4651b51369d7a47ab95a9b72c0aabd
algarfer <algarferx@proton.me>
2025-01-06 01:34:39 +0100

    Now when the day changes the tasks displayed in the menu are updated

--- apk/app/src/main/java/com/uniovi/melhouse/viewmodel/MenuFragmentViewModel.kt
+++ apk/app/src/main/java/com/uniovi/melhouse/viewmodel/MenuFragmentViewModel.kt
@@ -5,13 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.uniovi.melhouse.data.model.Task
 import com.uniovi.melhouse.data.repository.task.TaskRepository
-import com.uniovi.melhouse.utils.today
-import com.uniovi.melhouse.utils.tomorrow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,17 +23,38 @@ class MenuFragmentViewModel @Inject constructor(
     taskRepository: TaskRepository
 ) : ViewModel() {
 
-    val todayTasks: LiveData<List<Task>> = taskRepository.findAssignedByDateAsFlow(
-        today()
-    )
-        .catch { _genericError.postValue(it.message) }
-        .asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+    private val today = flow {
+        while (true) {
+            emit(LocalDate.now())
 
-    val tomorrowTasks: LiveData<List<Task>> = taskRepository.findAssignedByDateAsFlow(
-        tomorrow()
-    )
+            val now = LocalDateTime.now()
+            val nextMidnight = now.toLocalDate().plusDays(1).atStartOfDay()
+            val delay = Duration.between(now, nextMidnight).toMillis()
+
+            kotlinx.coroutines.delay(delay)
+        }
+    }
+    private val tomorrow = flow {
+        while (true) {
+            emit(LocalDate.now().plusDays(1))
+
+            val now = LocalDateTime.now()
+            val nextMidnight = now.toLocalDate().plusDays(1).atStartOfDay()
+            val delay = Duration.between(now, nextMidnight).toMillis()
+
+            kotlinx.coroutines.delay(delay)
+        }
+    }
+    private val _tasks = taskRepository.findAllAsFlow()
         .catch { _genericError.postValue(it.message) }
-        .asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+        .shareIn(viewModelScope, started = SharingStarted.Lazily)
+
+    val todayTasks = _tasks.combine(today) { tasks, date ->
+        tasks.filter { it.endDate == date }
+    }.asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+    val tomorrowTasks = _tasks.combine(tomorrow) { tasks, date ->
+        tasks.filter { it.endDate == date }
+    }.asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
 
     val genericError: LiveData<String?>
         get() = _genericError
